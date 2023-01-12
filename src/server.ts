3ad32417beb8ea11497e82d51c2f1dc6bccc5c05
Bruno Andrade <bhmandrade411@gmail.com>
2023-01-11 22:20:18 -0300

    Implementação do módulo user.

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { ForecastController } from './controller/forecast';
 import { Application } from 'express';
 import * as database from '@src/database';
 import { BeachesController } from './controller/beaches';
+import { UsersController } from './controller/users';
 
 export class SetupServer extends Server {
     constructor(private port = 6000) {
@@ -24,7 +25,8 @@ export class SetupServer extends Server {
     private setupControllers(): void {
         const forecastController = new ForecastController();
         const beachesController = new BeachesController();
-        this.addControllers([forecastController, beachesController]);
+        const usersController = new UsersController();
+        this.addControllers([forecastController, beachesController, usersController]);
     }
 
     private async databaseSetup(): Promise<void>{
@@ -37,5 +39,11 @@ export class SetupServer extends Server {
 
     public getApp(): Application {
         return this.app;
+    }
+
+    public start(): void {
+        this.app.listen(this.port, () => {
+            console.info('Server listening on port: ', this.port);
+        });
     }
 }
